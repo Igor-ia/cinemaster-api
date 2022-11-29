12e7ea020a365fad7952a95e8f53abb9565c66d0
Igor-ia <igorsantos-silva123@outlook.com>
2022-11-29 20:49:16 -0300

    deploy rest api on heroku

---
+++ build/server.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const movieRoute_1 = __importDefault(require("./routes/movieRoute"));
+const loginRoute_1 = __importDefault(require("./routes/loginRoute"));
+const favoriteRoute_1 = __importDefault(require("./routes/favoriteRoute"));
+const dislikeRoute_1 = __importDefault(require("./routes/dislikeRoute"));
+//Express configuration
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+//Routes
+app.use('/movies', movieRoute_1.default);
+app.use('/login', loginRoute_1.default);
+app.use('/favorites', favoriteRoute_1.default);
+app.use('/dislike', dislikeRoute_1.default);
+app.listen(3333);
